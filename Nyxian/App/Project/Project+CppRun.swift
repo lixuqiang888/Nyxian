569d3dbb2406e9917be1e8d68710f3ef079c3eb5
FridaDEV <157828245+fridakitten@users.noreply.github.com>
2025-04-06 22:24:31 +0200

    Moving back to the PoC to test more MachO compilation

--- Nyxian/App/Project/Project+CppRun.swift
+++ Nyxian/App/Project/Project+CppRun.swift
@@ -9,7 +9,7 @@ let fcm: FCMBridge = FCMBridge()
 
 func runCppAtPath(_ path: String) {
     let filePath: URL = URL(filePath: path)
-    //let outPath = filePath.deletingLastPathComponent().appendingPathComponent("output.o")
-    llvm.interpretProgram(filePath.path.data(using: .utf8)!)
-    //fcm.compileObject(filePath.path.data(using: .utf8)!, output: outPath.path.data(using: .utf8)!)
+    let outPath = filePath.deletingLastPathComponent().appendingPathComponent("output.o")
+    //llvm.interpretProgram(filePath.path.data(using: .utf8)!)
+    fcm.compileObject(filePath.path.data(using: .utf8)!, output: outPath.path.data(using: .utf8)!)
 }
