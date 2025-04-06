47efd23f5dac747e73116410401dd2b16621e7a2
FridaDEV <157828245+fridakitten@users.noreply.github.com>
2025-04-06 23:01:06 +0200

    adding clangs own include??

--- Nyxian/Runtime/FCM/FCMBridge.mm
+++ Nyxian/Runtime/FCM/FCMBridge.mm
@@ -39,8 +39,8 @@ NSString* NSStringFromCString(const char *text);
     output_file_path[fileName.length] = '\0';
 
     // Invoke the interpreter
-    const char* argv[] = { "clang", "-isysroot", [[NSString stringWithFormat:@"%@/iPhoneOS16.5.sdk", [[NSBundle mainBundle] bundlePath]] UTF8String], input_file_path, "-o", output_file_path};
-    CompileObject(6, argv, output_file_path);
+    const char* argv[] = { "clang", "-isysroot", [[NSString stringWithFormat:@"%@/iPhoneOS16.5.sdk",  [[NSBundle mainBundle] bundlePath]] UTF8String], [[NSString stringWithFormat:@"-I%@/include",  [[NSBundle mainBundle] bundlePath]] UTF8String], input_file_path, "-o", output_file_path};
+    CompileObject(7, argv, output_file_path);
 }
 
 @end /* implementation LLVMBridge */
